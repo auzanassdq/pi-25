ca426d993d50922b275d88f1cb0187db73f58a2f
auzanassdq <auzanassidqi@gmail.com>
2025-10-09 09:42:39 +0700

    mongoose

--- 22-express-mongoose/app.js
+++ 22-express-mongoose/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const allRoutes = require('./routes');
+const connectToDb = require('./config/db');
 
 const PORT = 3000 || process.env.PORT;
+
+// Connect to MongoDB
+connectToDb();
 
 app.use(express.json());
 app.get('/', (req, res) => {
